8e760036399191b2a1721973cb702a8f9dc14544
gcristini <gabriele.cristini@datalogic,com>
2020-10-09 17:19:32 +0200

    Structure of Main.py

---
+++ Sources/Libraries/Enums.py
@@ -0,0 +1,32 @@
+class Enums:
+    """    """
+   
+    # ------------------------------ #
+    # ---------- Main.py ----------- #
+    # ------------------------------ #
+    class MainStateEnum:
+        MAIN_STATE_INIT = "init"
+        MAIN_STATE_SC_OPEN = "sc_open"
+        MAIN_STATE_ADB_CONNECT = "adb_connect"
+        MAIN_STATE_RECORDER_APP_OPEN = "recorder_app_open"
+        MAIN_STATE_RUN_TEST = "run_test"
+        MAIN_STATE_STOP = "stop"
+        #MAIN_STATE_EXIT = "exit"
+        
+
+        @classmethod
+        def vars(cls):
+            return [name for name, value in vars(cls).items() if name.isupper()]
+
+        @classmethod
+        def values(cls):
+            return [value for name, value in vars(cls).items() if name.isupper()]
+
+
+if __name__ == "__main__":
+    test = Enums.MainStateEnum
+    print(type(test.vars()))
+    print(test.vars())
+    print(test.values())
+
+    print ("exit" in test.values())
--- Sources/Main.py
+++ Sources/Main.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python
 
+from Libraries.Enums import Enums as en
+
+
 class Main(object):
     """ """
 
@@ -9,65 +12,67 @@ class Main(object):
     def __init__(self):
         """ Constructor """
 
+        # State Machine
+        self._main_state_fun_dict = {
+            en.MainStateEnum.MAIN_STATE_INIT: self._init_state_manager,
+            en.MainStateEnum.MAIN_STATE_SC_OPEN: self._sc_open_state_manager,
+            en.MainStateEnum.MAIN_STATE_ADB_CONNECT: self._adb_connect_state_manager,
+            en.MainStateEnum.MAIN_STATE_RECORDER_APP_OPEN: self._recorder_app_state_manager,
+            en.MainStateEnum.MAIN_STATE_RUN_TEST: self._run_test_state_manager,
+            en.MainStateEnum.MAIN_STATE_STOP: self._stop_state_manager,
+            #en.MainStateEnum.MAIN_STATE_EXIT: self._exit_state_manager
+        }
+
+        self._main_state: None
+        self._last_main: None
+
     # ---------------------------------------------------------------- #
     # ----------------------- Private Methods ------------------------ #
     # ---------------------------------------------------------------- #
     @staticmethod
     def _print_help():
-        """"""        
-    pass
+        """"""
+        pass
 
-    # ******** State Machine Functions ******** #
+    # ------------------ STATE MACHINE ------------------ #
+    def _go_to_next_state(self, state):
+        """"""
+        # Store last state
+        self._last_main_state = self._main_state
+
+        # Go to next state
+        self._main_state = state
+        pass
+
     def _init_state_manager(self):
         """"""
-       
+        self._go_to_next_state(en.MainStateEnum.MAIN_STATE_SC_OPEN)
+        return
 
-    def _wait_state_manager(self):
+    def _sc_open_state_manager(self):
         """"""
-        # cmd = input("- Please enter a command: ")
+        self._go_to_next_state(en.MainStateEnum.MAIN_STATE_ADB_CONNECT)        
+        return
 
-        # if cmd == enum.MainAppCommands.MAC_RUN:
-        #     # Go to run state
-        #     self._main_state = enum.MainAppStatesEnum.MAS_RUN
-
-        # elif cmd == enum.MainAppCommands.MAC_HELP:
-        #     # Go to wait state
-        #     self._main_state = enum.MainAppStatesEnum.MAS_HELP
-
-        # elif cmd == enum.MainAppCommands.MAC_EXIT:
-        #     # Go to exit state
-        #     self._main_state = enum.MainAppStatesEnum.MAS_EXIT
-
-        # else:
-        #     # Go to help state
-        #     self._main_state = enum.MainAppStatesEnum.MAS_HELP
-        # pass
-
-    def _run_state_manager(self):
+    def _adb_connect_state_manager(self):
         """"""
-        print(cm.Fore.GREEN + cm.Style.DIM + "\n---------- Run Test ----------")
-        #self._TemperatureTestSX5_TC.run_test()
-        self._TemperatureTestSX5_TS.run_test()
+        self._go_to_next_state(en.MainStateEnum.MAIN_STATE_RECORDER_APP_OPEN)
+        return
 
-        # Go to Wait state
-        # self._main_state = enum.MainAppStatesEnum.MAS_WAIT
-
-        pass
-
-    def _help_state_manager(self):
+    def _recorder_app_state_manager(self):
         """"""
-        # # Print Help
-        # self._print_help()
+        self._go_to_next_state(en.MainStateEnum.MAIN_STATE_RUN_TEST)
+        return
 
-        # # Go to wait state
-        # self._main_state = enum.MainAppStatesEnum.MAS_WAIT
-        pass
-
-    def _exit_state_manager(self):
+    def _run_test_state_manager(self):
         """"""
-        print(cm.Fore.MAGENTA + "Goodbye!")
-        sys.exit()
-        pass
+        self._go_to_next_state(en.MainStateEnum.MAIN_STATE_STOP)
+        return
+
+    def _stop_state_manager(self):
+        """"""        
+        return
+    
 
     def _main_state_machine_manager(self):
         """"""
@@ -81,21 +86,34 @@ class Main(object):
     # ************************************************ #
     # **************** Public Methods **************** #
     # ************************************************ #
-    def main(self):
+
+    def init(self):
+        # Initialize Colorama library
+        #cm.init(autoreset=True)
+        
+        self._main_state = en.MainStateEnum.MAIN_STATE_INIT
+        self._last_main_state = en.MainStateEnum.MAIN_STATE_INIT
+
+
+    def run(self):
         """ Main Application """
 
-        # # Init
-        # self._init_state_manager()
+        # Init
+        self._init_state_manager()
 
-        # while not (self._main_state == enum.MainAppStatesEnum.MAS_EXIT and
-        #            self._last_main_state == enum.MainAppStatesEnum.MAS_EXIT):
+        while not (self._main_state == en.MainStateEnum.MAIN_STATE_STOP and
+                   self._last_main_state == en.MainStateEnum.MAIN_STATE_STOP):
 
-        #     # Store the last state machine state
-        #     self._last_main_state = self._main_state
+            # Store the last state machine state
+            self._last_main_state = self._main_state
 
-        #     # Run state machine at current state
-        #     self._main_state_machine_manager()
+            # Run state machine at current state
+            self._main_state_machine_manager()
 
 
-Main().main()
 
+
+if __name__ == "__main__":
+    test = Main()
+    test.init()
+    test.run()
